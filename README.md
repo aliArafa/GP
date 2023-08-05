b86afe2b5cde318f1bc7546a964680251956dcb5
alarafa <ali.arafa01@gmail.com>
2023-08-05 16:01:52 +0300

    adding script+ Readme + makefile

---
+++ README.md
@@ -0,0 +1,8 @@
+# Guessing Game
+
+> [NOTE]
+> The last run for make file was: 28 Aug  5 15:22
+
+> [NOTE]
+> The number of script lines is: 24
+
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,16 +1,24 @@
-numberOfFiles=$(ls | wc -l)
-echo $numberOfFiles
-echo "How many files are there in the current directory?"
-read yourGuess	
-while [[ "$yourGuess" -ne "$numberOfFiles" ]]; do
-    if [ "$yourGuess" -gt "$numberOfFiles" ]; then
+#this is a function to determine if number was high or low
+high_or_low() {
+    if [ "$1" -gt "$2" ]; then
         echo "Your guess is too high."
     else
         echo "Your guess is too low."
     fi
+}
+#counting the number of files
+numberOfFiles=$(ls | wc -l)
+
+#taking the guessing from the user
+echo "How many files are there in the current directory?"
+read yourGuess	
+
+#looping till we get the right answer 
+while [[ "$yourGuess" -ne "$numberOfFiles" ]]; do
+   
+    high_or_low "$yourGuess" "$numberOfFiles"
     echo "Please try again."
     read yourGuess
 done
 
 echo"congratulations, your guess is right the number of files is: $yourGuess"
-
